8129ec5ab0c93e3121630397269555e69c409f39
neymar <871361324@qq.com>
2020-09-12 17:01:41 +0800

    Initial Commit

---
+++ firdt/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  firdt
+//
+//  Created by hs on 2020/9/12.
+//  Copyright © 2020 hs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
