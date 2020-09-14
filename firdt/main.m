bc3d8f7f7b021d01cc4fddbda21ca7778ef44d3d
neymar <871361324@qq.com>
2020-09-14 20:28:10 +0800

    类声明

--- firdt/main.m
+++ firdt/main.m
@@ -7,11 +7,30 @@
 //
 
 #import <Foundation/Foundation.h>
+/*
+ 
+ */
+//编写类 NSObject具备创建对象
+@interface Iphone : NSObject {
+    float _model;
+    int _cpu;
+    double _size;
+    int _color;
+}
+@end
 
+//实现类
+@implementation Iphone
+
+<#methods#>
+
+@end
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
         NSLog(@"Hello, World!");
+        NSLog(@"Objective-C OK!");
+        Iphone *p = [Iphone new];
     }
     return 0;
 }
